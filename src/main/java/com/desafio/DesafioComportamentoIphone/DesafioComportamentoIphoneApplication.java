824abf5e43abd4e531aace9cd65b85cb24d3d4f3
Abmaellf <abmael.ninha@gmail.com>
2023-10-20 16:41:49 -0400

    first commit

---
+++ src/main/java/com/desafio/DesafioComportamentoIphone/DesafioComportamentoIphoneApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.DesafioComportamentoIphone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioComportamentoIphoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioComportamentoIphoneApplication.class, args);
+	}
+
+}
